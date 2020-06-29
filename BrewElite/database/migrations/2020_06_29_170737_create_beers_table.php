cce071511cd707bb647146bc91792a18109560ff
Prachi-sd <prachid196@gmail.com>
2020-06-29 22:52:16 +0530

    Created Initial Models And Migrations

---
+++ BrewElite/database/migrations/2020_06_29_170737_create_beers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->text('lable');
+            $table->smallInteger('rating');
+            $table->unsignedBigInteger('brewery_id');
+            $table->foreign('brewery_id')->references('id')->on('breweries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# LaravelProject
+# BrewElite
